e737c4a0475b6599100a8e036a170e42aac0de6c
EtienneAbengclear <abengetienne@gmail.com>
2023-10-14 00:56:49 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - It's for use puts
+ * main - How to use the fonction PUTS.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");         	return (0);
+	puts("Programming is like building a must a multilingual puzzle\n");
+	return (0);
 }
