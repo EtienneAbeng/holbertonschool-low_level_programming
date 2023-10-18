03f9db099fab24a8c12275680eb808c4c8008082
EtienneAbengclear <abengetienne@gmail.com>
2023-10-18 03:21:17 -0700

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - write a program that prints _putchar.
+ *
+ * Return: 0 (sucess).
+ */
+
+int main(void)
+{
+	char prints;
+
+	prints = putchar;
+
+	printf("%c\n", prints);
+
+	return (0);
+}
