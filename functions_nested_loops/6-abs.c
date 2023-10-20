ce7bc0e3da31886b66ba937fd0280c4d858a6582
EtienneAbengclear <abengetienne@gmail.com>
2023-10-19 22:54:08 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Abs - There is no such thing as absolute calue in this world.
+ *
+ * return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	else if (n == 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Last digit - A function that print the last digit of a number.
+ *
+ * Return: the value of the last digit.
+ */
+
+int print_last_digit(int n)
+{
+
+	if (n % 10 > 0)
+	{
+		_putchar(n);
+	}
+
+	else
+	{
+		_putchar(n);
+	}
+
+	return (n);
+}
