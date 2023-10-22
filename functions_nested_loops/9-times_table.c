04a7ebc95f8e5eb32075f1df3725e5469a8c76c6
EtienneAbengclear <abengetienne@gmail.com>
2023-10-22 03:15:09 -0700

    9_times

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,28 +4,32 @@
  *
  * return : Always 0.
  */
-oid times_table(void)
+void times_table(void)
 {
-    int i, j;
-
-    for (i = 0; i <= 9; i++) {
-        for (j = 0; j <= 9; j++) {
-            int result = i * j;
-
-            if (result < 10) {
-                putchar(' '); // Ajoute un espace pour la lisibilité (chiffre des unités)
-                putchar(result + '0'); // Affiche le résultat en tant que caractère
-            } else {
-                putchar((result / 10) + '0'); // Affiche le chiffre des dizaines
-                putchar((result % 10) + '0'); // Affiche le chiffre des unités
-            }
-
-            if (j < 9) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-
-        putchar('\n');
-    }
+	int i, j;
+	for (i = 0; i <= 9; i++) 
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int result = i * j;
+			
+			if (result < 10)
+			{
+				putchar(' '); // Ajoute un espace pour la lisibilité (chiffre des unités)
+				putchar(result + '0'); // Affiche le résultat en tant que caractère
+			}
+			else
+			{
+				putchar((result / 10) + '0'); // Affiche le chiffre des dizaines
+				putchar((result % 10) + '0'); // Affiche le chiffre des unités
+			}
+			if (j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		
+		putchar('\n');
+	}
 }
