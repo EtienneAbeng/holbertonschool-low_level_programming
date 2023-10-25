35bf9c9036cd8c7330c9cfb84624d630368c58fb
EtienneAbengclear <abengetienne@gmail.com>
2023-10-25 06:51:57 -0700

    correction

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 98 Battery st - A pointer to an int as parameter and updates the value.
+ * reset_to_98 Battery st - A pointer to an int as parameter and updates the value.
  *
  * @n: A pointeur to variable.  
- * 
  * 
  */
 void reset_to_98(int *n)
