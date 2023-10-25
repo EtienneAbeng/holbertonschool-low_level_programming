ae07b755e2704ef586379d8237c1cade16cea47b
EtienneAbengclear <abengetienne@gmail.com>
2023-10-25 07:06:37 -0700

    correction

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,9 +3,11 @@
 /**
  * Swap - A function that swaps the values of two integer.
  *
- * @swap - interchange the variable
- * @'a' - parameter 'a' take a value of 'b'.
- * @'b' - parameter 'b' take a value of 'a'.
+ * @swap - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: pointer to the second integer.
+ *
+ * This function swaps the values of the two integer pointed.
  */
 
 void swap_int(int *a, int *b)
