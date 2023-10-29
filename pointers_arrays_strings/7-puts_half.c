ad43a12acabaeb6412cca5ad180ae5c5af348d42
EtienneAbengclear <abengetienne@gmail.com>
2023-10-29 03:33:26 -0700

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,13 +17,8 @@ void puts_half(char *str)
 	}
 
 	/*Calculate the starting positon for the second half.*/
-	/* the starting position for the second half, from the 6th.*/
+	/*the starting position for the second half, from the 6th.*/
 	if (length % 2 == 0)
-	{
-		half = length / 2;
-	}
-
-	else 
 	{
 		half = (length - 1) / 2;
 	}
