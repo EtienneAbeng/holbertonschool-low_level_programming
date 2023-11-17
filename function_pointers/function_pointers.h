c59eb3baebb52ae670d0152fb2750b367c78c8b0
EtienneAbengclear <abengetienne@gmail.com>
2023-11-17 12:45:09 -0800

    function_pointer

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
