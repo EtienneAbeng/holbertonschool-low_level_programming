61a57e3b5fa89bfddba256c342a3871eb93fff09
EtienneAbengclear <abengetienne@gmail.com>
2023-12-08 09:19:53 -0800

    hash_table_create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,12 +11,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-       	hash_table_t *new_table = NULL;
-       
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
+
 	/* Allocate memory for the hash table structure */
-       	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
 	/* Allocate memory for the array of pointers (buckets) */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
@@ -24,13 +26,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);  /* If allocation fails, free previously allocated memory */
 		return (NULL);
 	}
-	
+
 	/* Initialize each bucket to NULL */
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		new_table->array[i] = NULL;
 	}
 	
 	new_table->size = size;  /* Set the size of the hash table */
+
 	return (new_table);
 }
