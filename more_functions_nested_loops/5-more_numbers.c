9cba86c4668838d85078d0d8e7810fc2ddb0bfd4
EtienneAbengclear <abengetienne@gmail.com>
2023-12-21 14:44:46 -0800

    Update exercice

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * more_numbers - prints 10 times the numbers 0 to 14
- * @_putchar: Only three times in the code
  * Return: Always 0.
  */
 
@@ -10,10 +9,10 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j < 15 ; j++)
-	{		
+	{
 		if (j >= 10)
 		{
 			_putchar((j / 10) + '0');
