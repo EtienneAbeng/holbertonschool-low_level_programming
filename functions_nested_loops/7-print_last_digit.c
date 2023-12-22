3abc6cc59e04bbad4e3c7a5b4ff6aee039b7a63c
EtienneAbengclear <abengetienne@gmail.com>
2023-12-22 00:13:30 -0800

    Update exercice

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -16,10 +16,5 @@ int print_last_digit(int n)
 		_putchar('0' + n % 10);
 	}
 
-	else if ((n == 0) || (n > 0))
-	{
-		_putchar('0' + n % 10);
-	}
-
 	return (n % 10);
 }
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,27 +1,41 @@
 #include "main.h"
 /**
- * Time table - A function that prints the 9 times, starting with 0
+ * times_table - Prints the multiplication table for 9, starting from 0
  *
- * return: void.
+ * Description: This function displays the result of multiplying numbers
+ * from 0 to 9 by each other in a tabular format. The result is printed to
+ * the standard output. The function takes no arguments and returns void.
  */
 void times_table(void)
 {
-	int i, j;
+	int i;
+	int j;
+	int result;
+	
 	for (i = 0; i <= 9; i++) 
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int result = i * j;
-			
+			result = i * j;
+
 			if (result < 10)
 			{
-				_putchar(' '); /* Ajoute un espace pour la lisibilité (chiffre des unités)*/
-				_putchar(result + '0'); /* Affiche le résultat en tant que caractère*/
+				if (result < 10)
+				{
+				/* Ajoute un espace pour la lisibilité (chiffre des unités)*/
+				_putchar(' ');
+				_putchar(' ');
+				}
+				_putchar (0);
+				/* Affiche le résultat en tant que caractère*/
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar((result / 10) + '0'); /*Affiche le chiffre des dizaines*/
-				_putchar((result % 10) + '0'); /* Affiche le chiffre des unités*/
+				 /*Affiche le chiffre des dizaines*/
+				_putchar((result / 10) + '0');
+				/* Affiche le chiffre des unités*/
+				_putchar((result % 10) + '0');
 			}
 			if (j < 9)
 			{
