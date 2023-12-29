b2024e1d5427de7f2b24f7dad8bde1e29b59d0ba
EtienneAbeng <abengetienne@gmail.com>
2023-12-29 10:49:44 -0800

    first commit

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	/* Check if n is greater than 1 */
+	if (n > 1)
+	{
+	/*Recursievely call the function to print the remaining bits*/
+		print_binary(n >> 1);
+	}
+	
+	/* Print the least significant bit of n */
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+
+	else 
+	{
+		_putchar('0');
+	}
+}
